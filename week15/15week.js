05cd8bf4e9ceb78e0cc5312253f8f097d938d0cc
Yana <satzkevitch2015@yandex.ru>
2022-05-18 21:09:32 +0300

    задание 2 готово

--- week15/15week.js
+++ week15/15week.js
@@ -13,6 +13,8 @@ function background()
   document.getElementById ("background").style.backgroundColor = "yellow";
 }
 /*Задание 2*/
+    let isFormValid = true;
+
     function check()
     {
         let name = document.getElementById ("inputName");
@@ -29,35 +31,44 @@ function background()
         document.getElementById ('errorPhone').innerHTML = '';
         if (name.value == '') {
           document.getElementById ('errorName').innerHTML+= "Пожалуйста, укажите имя<br>";
+          isFormValid = false;
         }
         if (name2.value == '') {
           document.getElementById ('errorName2').innerHTML+= "Пожалуйста, укажите фамилию<br>";
+          isFormValid = false;
         }
         if (login.value == '') {
           document.getElementById ('errorLogin').innerHTML+= "Необходимо выбрать логин<br>";
+          isFormValid = false;
         }
         if (password1.value == '') {
           document.getElementById ('errorPassword1').innerHTML+= "Необходимо выбрать пароль<br>";
+          isFormValid = false;
         } else if (password1.value.length <8) {
           document.getElementById ('errorPassword1').innerHTML+= "Ваш пароль слишком короткий<br>";
+          isFormValid = false;
         }
         if (password2.value == '') {
           document.getElementById ('errorPassword2').innerHTML+= "Необходимо повторить пароль<br>";
-        } else if (password2.value != password1) {
+          isFormValid = false;
+        } /*else if (password2.value != password1) {
           document.getElementById ('errorPassword2').innerHTML+= "Пароль не совпадает<br>";
-        }
+          isFormValid = false;  не получилось сравнить пароли, при одинаковых поролях все равно возвращает ошибку 
+        }*/
         if (phone.value == '') {
           document.getElementById ('errorPhone').innerHTML+= "Пожалуйста, укажите номер телефона<br>";
+          isFormValid = false;
         }
+        return isFormValid;
     }
 
     function nameCheck() {
-      const name= document.getElementById ('inputName').value;
-      const name2= document.getElementById ('inputName2').value;
-      alert(`Добро пожаловать, ${name} ${name2}!`);
+      if (isFormValid == true) {
+      const nameHello= document.getElementById ('inputName').value;
+      const name2Hello= document.getElementById ('inputName2').value;
+      alert(`Добро пожаловать, ${nameHello} ${name2Hello}!`);
+      }
     }
 
     button.addEventListener("click", check);
     button.addEventListener("click", nameCheck);
-
-    //еще не завершен
