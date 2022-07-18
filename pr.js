214bdb5640cbd934d3ed5ac2df079a5a345376dc
Yana <satzkevitch2015@yandex.ru>
2022-07-18 11:41:54 +0300

    super

--- pr.js
+++ pr.js
@@ -1,8 +1,33 @@
-const person = {
-    name: "valera",
-    age: 23
+let json = `[{
+    "title": "bbbb",
+    "genre": "hhhhh",
+    "author": "GGGG"
+},
+{
+    "title": "bbbb",
+    "genre": "hhhhh",
+    "author": "GGGG",
+    "image": "https://lt3.pigugroup.eu/uploaded/shutterstock_509582812-min.jpg"
+},
+{
+    "title": "bbbb",
+    "genre": "hhhhh",
+    "author": "GGGG"
 }
-person.city = "Amsterdam";
-
-
-console.log (person)
+]`;
+document.addEventListener ( "DOMContentLoaded", function 
+(event) {
+    let books = JSON.parse (json);
+    console.log(books);
+    let booksContent = "";
+    for (let book of books)
+    {
+        booksContent += `<div class='gero'> 
+        <h2> ${book.title}) </h2>
+        <div> Вселенная: ${book.genre} </div>
+        <div> Альтер эго:  ${book.author} </div>
+        <div> Изображение:  ${book.image} </div>
+        </div>`
+    }
+    document.getElementById("geroContainer").innerHTML =  booksContent;
+});
