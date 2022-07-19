23d74fa2106410ca5069a164b6b6e3b1577e1379
Yana <satzkevitch2015@yandex.ru>
2022-07-19 20:22:57 +0300

    ready

--- week20/api.js
+++ week20/api.js
@@ -1,2 +1,21 @@
-fetch('https://api.github.com/users/YanaRashetskaya')   .then(response   =>
-console.log(response.json())) .catch(err => console.log(err));
+
+document.addEventListener ("DOMContentLoaded", function 
+(event) {
+    getWeather();
+});
+
+function  getWeather(){
+    let city = document.getElementById('searchCity').value;
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' +city+ '&appid=c34567eb30db0fabf3b0cdfcef1067d1&units=metric')
+    .then(response => response.json())
+    .then (weather => {
+        console.log (weather);
+        document.querySelector('.city').innerHTML = weather.name;
+        document.querySelector('.temp')
+        .innerText = Math.round( weather.main.temp);
+        document.querySelector('.hi-low').innerText = 
+        Math.round( weather.main.temp_max) + "/" + Math.round( weather.main.temp_min);
+        document.querySelector('.weather').innerText = weather.weather[0].description; 
+});
+
+}
